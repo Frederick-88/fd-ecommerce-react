3c63891b9ee741a008d34b9345324e7cd52d5844
Frederick-88 <frederickfd88@gmail.com>
2020-06-17 14:32:20 +0700

    Done Footer

--- src/components/UserPages/Index.js
+++ src/components/UserPages/Index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import mainBg from "../../assets/mainBackground.png";
 import logo from "../../assets/logo.png";
+import logoHead from "../../assets/logo-1.png";
+import logoText from "../../assets/logo-2.png";
 import Carousel from "./Carousel";
 import ProductField from "./Products";
 import "../Users.css";
@@ -138,7 +140,71 @@ const Index = () => {
       {/* part 3 - content */}
 
       {/* Footer */}
-      {/* <div>Footer</div> */}
+
+      <hr className="horizontal-line" />
+
+      <div className="mx-5 my-5">
+        <div className="row">
+          <div className="col-md-2">
+            <img src={logoHead} alt="..." className="w-100" />
+          </div>
+          <div className="col-md-3">
+            <p className="footer-title">S2 BOUTIQUE</p>
+            <small>
+              Look inside trendy,elegant & fantastic fashion in Batam. T ell the
+              world who you are with style without having to speak.
+            </small>
+            <div className="mt-4">
+              <small className="pt-4">© 2020 S2-Boutique Batam</small>
+            </div>
+          </div>
+
+          <div className="col-md-3">
+            <p className="footer-title">REACH OUT</p>
+            <button className="btn btn-outline-success mr-3">
+              <i class="fab fa-whatsapp fa-lg" />
+            </button>
+
+            <button className="btn btn-outline-instagram mr-3">
+              <i class="fab fa-instagram fa-lg" />
+            </button>
+            <button className="btn btn-outline-primary mr-3">
+              <i class="fab fa-facebook-square fa-lg" />
+            </button>
+
+            <button className="btn btn-outline-danger">
+              <i class="far fa-envelope fa-lg" />
+            </button>
+          </div>
+          <div className="col-md-4">
+            <p className="footer-title">SUBSCRIBE</p>
+            <small>
+              Don't miss out the Latest Sale, Discounts, and New Arrivals !!
+            </small>
+            <div class="input-group mt-3 mb-2">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Your Gmail/Whatsapp"
+                aria-label="Recipient's username"
+                aria-describedby="button-addon2"
+              />
+              <div class="input-group-append">
+                <button
+                  class="btn btn-outline-success"
+                  type="button"
+                  id="button-addon2"
+                >
+                  <i class="fas fa-chevron-circle-right"></i>
+                </button>
+              </div>
+            </div>
+            <small className="text-secondary">
+              We will not share your gmail/whatsapp to anyone else.
+            </small>
+          </div>
+        </div>
+      </div>
       {/* Footer */}
     </div>
   );
